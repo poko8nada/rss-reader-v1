22b0726ce5ba7fd0f0a2edb9cff34ed65c2d8f40
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-10 15:55:44 +0900

    Add footer component and integrate it into the root layout

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
+      {`© ${new Date().getFullYear()} PokoHanada`}
+    </footer>
+  )
+}
