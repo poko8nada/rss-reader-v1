5b43fed159b8d5bd3c278c4cdbbf341bb3ad562b
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-10 18:43:09 +0900

    Refactor home page to utilize custom hooks for feed management and implement chunked loading of articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,56 @@
 'use client'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import useArticle from '@/hooks/useArticle'
+import useFeed from '@/hooks/useFeed'
+import { useState } from 'react'
 import HatenaList from '../components/hatenaList'
 import Modal from '../components/modal'
 import OverlayLoading from '../components/overlayLoading'
-import type { hatenaItems } from './api/hatena/route'
+
+import type { hatenaItems } from '@/app/api/hatena/route'
 
 export default function Home() {
-  const [loading, setLoading] = useState(true)
-  const [feed, setFeed] = useState<hatenaItems[]>([])
-  const [article, setArticle] = useState<hatenaItems | null>(null)
+  const { feedChunk: hatenaFeed, loading: hatenaLoading } =
+    useFeed('/api/hatena')
+  const { article, setArticle } = useArticle()
 
-  useEffect(() => {
-    const fetchFeed = async () => {
-      setLoading(true)
-      const response = await fetch('/api/hatena')
-      const data = await response.json()
-      setFeed(data)
-      setLoading(false)
-    }
-    fetchFeed()
-  }, [])
+  const [chunk, setChunk] = useState([] as hatenaItems[])
+  const [chunkIndex, setChunkIndex] = useState(0)
 
-  useEffect(() => {
-    if (article) {
-      document.body.classList.add('overflow-hidden')
-    } else {
-      document.body.classList.remove('overflow-hidden')
-    }
-  }, [article])
+  const handleNext = (feed: hatenaItems[][]) => {
+    if (!feed[chunkIndex + 1]) return
+    setChunkIndex(chunkIndex + 1)
+    const nextChunk = [chunk, ...feed[chunkIndex + 1]].flat()
+    setChunk(nextChunk)
+  }
 
+  console.log(chunk)
   return (
     <div className={'max-w-5xl mx-auto w-full'} style={{ minHeight: '96vh' }}>
-      {loading && <OverlayLoading />}
+      {hatenaLoading && <OverlayLoading />}
       {article && <Modal article={article} setArticle={setArticle} />}
       <main className={'grid md:grid-cols-2 grid-flow-row'}>
-        {feed.map((item, index) => (
+        {hatenaFeed[0]?.map((item, index) => (
           <HatenaList
             key={`${index}-${item.title}`}
             item={item}
             setArticle={setArticle}
           />
         ))}
+        {chunk?.map((item, index) => (
+          <HatenaList
+            key={`${index}-${item.title}`}
+            item={item}
+            setArticle={setArticle}
+          />
+        ))}
+        <button
+          type='button'
+          disabled={!hatenaFeed[chunkIndex + 1]}
+          onClick={() => handleNext(hatenaFeed)}
+          className={`${!hatenaFeed[chunkIndex + 1] ? 'opacity-50 cursor-not-allowed' : ''} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-60 mx-auto my-4`}
+        >
+          {!hatenaFeed[chunkIndex + 1] ? 'No more feed' : 'Next'}
+        </button>
       </main>
     </div>
   )
